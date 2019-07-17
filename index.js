21bc1e00d5462dd82afa09c697607433c36e7a80
Kenneth Sweet <kenny.sweet@hotmail.com>
2019-07-17 18:32:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const eventDetails = document.querySelector(".eventDetails");
+
+// get event details
+const getEvents = (data) => {
+
+	let html = '';
+	data.forEach(doc => {
+		const event = doc.data();
+		const div = `
+			<div>
+				<div class="panel-heading" role="tab">
+					<h2 class="panel-title">
+						<a role="button" data-toggle="collapse" href="#${event.id}" data-parent="#accordion" aria-expanded="false" aria-controls="#${event.id}">${event.date} - ${event.city}</a>
+					</h2>
+				</div>
+				<div class="panel-collapse collapse" id="${event.id}" role="tabpanel">
+					<div class="panel-body">
+						<p>City: ${event.city}</p>
+						<p>Time: ${event.time}</p>
+						<p>Meeting Location: ${event.location}</p>
+						<p>On-Site Supervisor: ${event.supervisor}</p>
+					</div>
+				</div>
+			</div>
+		`;
+
+		html += div
+	
+	});
+
+	eventDetails.innerHTML = html;
+}
+
+// Conditional Menu Items
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+	if (user){
+		//toggle UI elements
+		loggedInLinks.forEach(item => item.style.display ='block');
+		loggedOutLinks.forEach(item => item.style.display ='none');
+	} 
+	else {
+		loggedInLinks.forEach(item => item.style.display ='none');
+		loggedOutLinks.forEach(item => item.style.display ='block');
+	}
+}
+
